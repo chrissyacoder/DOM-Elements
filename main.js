c71fbf110a1f1a8291ed4e3f9f7859515e422969
Christie Pamphile <cpam2011@yahoo.com>
2021-05-14 15:44:24 -0400

    modified assignment

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-    // 1 //
-document.getElementById("main-heading").innerHTML = "New DOM Layout";
+const mainHeader = document.querySelector('#main-heading');
+const para1 = document.querySelector('#para1');
+const para2 = document.querySelector('#para2');
+const para3 = document.querySelector('#para3');
+const para4 = document.querySelector('#para4');
+const btn = document.querySelector("#btn");
+const red = document.querySelector('#red div');
+const blue = document.querySelector("#blue div");
+const yellow = document.querySelector('#yellow div');
+const green = document.querySelector('#green div');
+const black = document.querySelector('#black div');
 
-//2//
-document.getElementById("page-header").classList.add("bg-success");
 
-    //3//
-document.getElementById("para1").textContent = "The Document Object (DOM) is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure."
 
-document.getElementById("para2").textContent = "The Document Object (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page. "
+mainHeader.textContent = 'New DOM Layout';
+mainHeader.classList.add('bg-success');
+para1.textContent = 'The Document Object Model (DOM) is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure';
+para2.textContent = 'The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.'
+btn. addEventListener('click', function(){
+    const p = document. createElement('p');
+    para4.append(p);
+    para4.append('The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document.');
+})
 
-    //4//
-  // I couldn't figure out how to get the click me feature to work//
-    function myFunction () {
-        var new_p = document.createElement("p")
-        new_p.textContent = "The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document."
-    }
-
-    //5//
-    document.getElementById('red').classList.remove('bg-white');
-    document.getElementById('red').classList.add('bg-danger');
-
-    document.getElementById("blue").classList.remove("bg-white");
-    document.getElementById("blue").classList.add("bg-primary");
-
-    document.getElementById("yellow").classList.remove("bg-white");
-    document.getElementById("yellow").classList.add("bg-warning");
-
-    document.getElementById("green").classList.remove("bg-white");
-    document.getElementById("green").classList.add("bg-success");
-
-    document.getElementById("black").classList.remove("bg-white");
-    document.getElementById("black").classList.add("bg-dark");
+red.classList.add('bg-danger');
+blue.classList.add('bg-primary');
+yellow.classList.add('bg-warning');
+green.classList.add('bg-success');
+black.classList.add('bg-dark');
+black.style.color = 'white';
